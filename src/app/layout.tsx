24a46fd618ba5ebdde57a383d9d2baa61c4d10ea
Unieggy <uniegg132@gmail.com>
2026-04-03 23:56:22 -0700

    modified favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,13 @@ export const metadata: Metadata = {
   title: "Zeyu (Michael) Lai",
   description:
     "Undergraduate Student interested in imitation learning, reinforcement learning, and generalist policy learning.",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
